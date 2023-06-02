a282ad553231b3ad1b1f288c17f6acfeab719627
NikoDadfarma <nikou.engineer@gmail.com>
2023-06-02 15:23:26 +0330

    take comment

--- tamrin1.py
+++ tamrin1.py
@@ -1,6 +1,10 @@
 import re
+
+"""this code check password at least 8 charachter (at least one captal)(at least one small)(at least one number)"""
+
 user_pass = input('enter your password: ')
 regex = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$"
+
 if re.match(regex, user_pass):
     print("it is right password")
 else:
